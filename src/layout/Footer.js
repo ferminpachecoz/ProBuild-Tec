2b1213d8fca7f80b21c8c08b48506035ca34d3c4
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2024-10-24 21:21:32 -0300

    h2

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae pharetra nibh.</p>
         </div>
         <div className='col-12 col-lg menu'>
-          <h4>MENÚ RÁPIDO</h4>
+          <h2>MENÚ RÁPIDO</h2>
           <ul>
             <li><Link aria-label="Link to Home page" to="/"></Link></li>
             <li><Link aria-label="Link to About Us section" to="/">Nosotros</Link></li>
@@ -19,12 +19,12 @@ export default function Footer() {
           </ul>
         </div>
         <div className='col-12 col-lg contact'>
-          <h4>CONTACTO</h4>
+          <h2>CONTACTO</h2>
           <p>Mail: loremipsum@gmail.com</p>
           <p>Télefono: 11-2233-4455</p>
         </div>
         <div className='col-12 col-lg'>
-          <h4>SEGUINOS</h4>
+          <h2>SEGUINOS</h2>
           <Link to="/" aria-label='Link to our WhatsApp through website' className='icon-a'><i className="bi bi-whatsapp"></i></Link>
         </div>
       </div>
